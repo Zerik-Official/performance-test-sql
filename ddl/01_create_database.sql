6cca34e49382d0caf7ab2891341d2246735efb0f
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-07-06 09:36:40 -0500

    feat: add sql script to create database and show tables

---
+++ ddl/01_create_database.sql
@@ -0,0 +1,8 @@
+-- Create the database
+create database if not exists bd_gustavo_guzman_micaela;
+
+-- Use the newly created database.
+use bd_gustavo_guzman_micaela;
+
+-- Show existing tables
+show tables;
